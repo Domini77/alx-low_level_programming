fc00be121c77a22b7a5f3bbcd3ee063c7b717eb0
Dominic <ugwudominic77@gmail.com>
2023-09-14 20:42:41 +0000

    Uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,18 +6,12 @@
  */
 int _isupper(int c)
 {
-	int i;
-
-	for (i = 'A'; i <= 'Z'; i++)
+	if (c <= 65 && c <= 90)
 	{
-		if (c == i)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
