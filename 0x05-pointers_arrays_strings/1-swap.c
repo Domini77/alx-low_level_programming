d2dd64c786d7103d35db688bb785ac1a2dc68249
Dominic <ugwudominic77@gmail.com>
2023-10-02 18:34:11 +0000

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Swap the values of two integers
+ * @a: first integer
+ * @b: Second integer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int i = 0;
+
+	i = *a;
+	*b = *a;
+	i = *b;
+}
